49149baac35b3d23887e6a8f29909fd0605adec9
noman122official <67466365+noman122official@users.noreply.github.com>
2022-10-20 20:30:31 +0530

    added grid view and card component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import GlobalStyle from "../styles/globalStyles";
 
+
+// swiper
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+  const theme = {
+    colors: {
+      primary: "#F26A7E",
+      secondary: {
+        1: "#69DE5D",
+        2: "#FF5C5C",
+        3: "#395185",
+      },
+    },
+    fontSize: {
+      h1: "42px",
+      h2: "32px",
+      h3: "28px",
+      h4: "20px",
+      h5: "18px",
+      h6: "16px",
+      h7: "14px",
+      h8: "12px",
+    },
+  };
+
+  return (
+    <>
+
+            <GlobalStyle />
+            <Component {...pageProps} />
+
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
